845582736a1124063665dfe3126674780155fb23
AnyaLina <106166162+YinLina@users.noreply.github.com>
2024-04-09 15:05:14 +0700

    first commit

---
+++ src/pages/user/UserProfile.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page padding class="page-container">
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4 q-pa-md">
+        <q-card square flat bordered>
+
+          <q-card class="my-card bg-red">
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>User Name</q-item-label>
+                <q-item-label caption>Student</q-item-label>
+              </q-item-section>
+            </q-item>
+
+          </q-card>
+
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-12 col-sm-6 col-md-8 q-pa-md">
+        <q-card square flat bordered>
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss"></style>
