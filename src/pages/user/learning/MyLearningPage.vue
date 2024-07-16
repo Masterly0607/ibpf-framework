8be8d094a5c860849085b93deb8e3dac455c4bb5
TBPanha11 <novpanha@gmail.com>
2024-07-16 17:48:47 +0700

    IBF 160724

--- src/pages/user/learning/MyLearningPage.vue
+++ src/pages/user/learning/MyLearningPage.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page padding class="ibf-container-1200">
-    <q-heading-1 icon="mdi-book-open" title="My Learning" />
+    <q-heading-1
+      icon="mdi-book-open"
+      title="My Learning"
+      subtitle="All your courses"
+    />
     <q-separator spaced />
   </q-page>
 </template>
