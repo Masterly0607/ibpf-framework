85bbe97370ac126926862a0f250dbbe7bf91329e
AnyaLina <106166162+YinLina@users.noreply.github.com>
2024-09-17 15:42:52 +0700

    17_09_24_fix_order_history_page

--- src/pages/user/order/OrderDashboardPage.vue
+++ src/pages/user/order/OrderDashboardPage.vue
@@ -12,56 +12,77 @@
 
     <!-- Order Details Box -->
     <q-card
-      bordered
-      class="q-mb-md ibf-card-3"
+      class="ibf-card-4 q-mb-md"
       v-for="(orderItem, orderItemIndex) in orderedPaginate.data"
       :key="orderItemIndex"
-      
     >
-      <q-card-section class="row items-center q-col-gutter-xl">
-        <!-- Order ID -->
-        <div class="col-2">
-          <span class="text-weight-bold text-subtile1">
-            #{{ orderItem.id }}</span
-          >
-        </div>
-
-        <!-- Product -->
-        <div class="col-3 ">
-          <div class="ellipsis-3-lines">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla vero
-            expedita exercitationem, inventore veritatis deserunt commodi
-            eveniet mollitia ex eos atque impedit rem modi ipsum aperiam? Rerum
-            ut vitae doloribus!
-          </div>
-        </div>
-
-        <!-- Total Amount -->
-        <div class="col-3  text-right  ">
-          <span class="text-red-8 ">
-            <price-original :price="orderItem.total_price" />
-          </span>
-        </div>
-
-        <!-- Order Status -->
-        <div class="col text-right">
-          <q-chip color="orange">
-            <div class="ellipsis">
-              {{ orderItem.payment_status }}
+      <q-card-section>
+        <q-item class="q-pa-none q-px-sm">
+          <!-- order id -->
+          <q-item-section side top>
+            <div class="column q-gutter-y-md">
+              <q-icon
+                size="30px"
+                color="primary"
+                name="mdi-invoice-list-outline"
+              />
+              <div class="ibf-h11 text-black text-weight-medium">
+                #{{ orderItem.id }}
+              </div>
             </div>
-          </q-chip>
-        </div>
-      </q-card-section>
+          </q-item-section>
 
-      <q-separator spaced size="3px" />
-      <!-- Time of order -->
-      <q-card-section class="row q-pt-none q-pb-sm justify-end text-grey">
-        <div class="text-caption">
+          <!-- product_title , price , & status -->
+          <q-item-section>
+            <q-item-label class="text-right q-py-sm">
+              <div class="ibf-h12 text-grey-6">
+                {{ orderItem.total_item }} Item(s)
+              </div>
+            </q-item-label>
+
+            <div class="row justify-between">
+              <div
+                v-for="(item, itemIndex) in orderItem.order_items"
+                :key="itemIndex"
+                class="q-my-md col-6"
+              >
+                <div class="ibf-h12 text-grey-10 ellipsis-2-lines text-italic">
+                  {{ item.course.title }}
+                </div>
+              </div>
+
+              <div class="column q-gutter-sm">
+                <price-original :price="orderItem.total_price" />
+                <div class="text-right">
+                  <q-chip
+                    size="12px"
+                    :color="
+                      checkPaymentStatusColor(orderItem.payment_status).color
+                    "
+                    text-color="white"
+                    :icon="
+                      checkPaymentStatusColor(orderItem.payment_status).icon
+                    "
+                  >
+                    <div class="text-capitalize">
+                      {{ orderItem.payment_status }}
+                    </div>
+                  </q-chip>
+                </div>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+
+        <q-separator spaced="8px" />
+
+        <div class="ibf-h12 text-grey-6 q-px-sm text-right">
           {{ formatDate(orderItem.created_at) }}
         </div>
       </q-card-section>
     </q-card>
 
+    <!-- old card -->
     <!-- <div class="column q-gutter-md">
       <q-card
         class="ibf-card-3"
@@ -139,15 +160,6 @@
           </div>
         </q-card-actions>
       </q-card>
-
-     
-
-      <q-btn
-        color="primary"
-        icon="check"
-        label="OK"
-        @click="fetchOrderedItems"
-      />
     </div> -->
   </div>
 </template>
